15dc239da308757cd5bc8fb4a851c875630cb6cf
Isaque-TW <isaque.ribeiro@thoughtworks.com>
2024-02-20 12:26:28 -0300

    Using test-driven development to create the Family Cash Card REST endpoint: a GET that returns a cashcard of a given ID.

--- src/test/java/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/example/cashcard/CashcardApplicationTests.java
@@ -42,4 +42,15 @@ class CashcardApplicationTests {
 		Double amount = documentContext.read("$.amount");
 		assertThat(amount).isEqualTo(123.45);
 	}
+
+	@Test
+	void shouldNotReturnACashCardWithAnUknownId() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/1000", 
+		String.class);
+		
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+		assertThat(response.getBody()).isBlank();
+	
+	}
 }
+
